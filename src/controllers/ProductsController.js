45b1d8d730a4bae499a804aefffcd666d7229843
Raphael Vizoni do Prado <ravizoni@gmail.com>
2022-06-29 23:34:35 -0300

    fix attribute items in correct format

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -19,9 +19,26 @@ module.exports = {
 				const categories = categoriesObj.values[0].path_from_root;
 				const categoryList = [].concat(categories).map(({ name }) => name);
 				const products = JSON.parse(data).results;
+				const items = [].concat(products).map((prod) => {
+					const splitDecimalAmountPrice = prod.prices.prices[0].amount
+						.toString()
+						.split(".");
+					return {
+						id: prod.id,
+						title: prod.title,
+						picture: prod.thumbnail,
+						condition: prod.condition,
+						free_shipping: prod.shipping.free_shipping,
+						price: {
+							currency: prod.prices.prices[0].currency_id,
+							amount: Number(splitDecimalAmountPrice[0]),
+							decimals: Number(splitDecimalAmountPrice[1]),
+						},
+					};
+				});
 				const returnedObject = JSON.stringify({
 					categories: categoryList,
-					items: products,
+					items: items,
 				});
 				response.writeHead(200, { "Content-Type": "application/json" });
 				response.end(returnedObject);
