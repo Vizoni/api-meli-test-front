82320e502ab372189cdd519fb558bf1864e4b3cf
Raphael Vizoni do Prado <ravizoni@gmail.com>
2022-07-02 13:45:56 -0300

    refactor: cors middleware

--- src/index.js
+++ src/index.js
@@ -7,15 +7,8 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 const app = express();
 
-const middleWareCors = (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*")
-	res.header("Access-Control-Allow-Methods", "GET, OPTIONS")
-	res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
-	app.use(cors())
-	next()
-}
-app.use(middleWareCors)
 app.use(express.json())
+app.use(cors())
 app.use(routes);
 
 
